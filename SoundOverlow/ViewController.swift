5b2c62d00ee8f4c608fea13300525982958cecd6
Tanja Stroble <plusdeltah@gmail.com>
2018-01-03 10:23:25 -0800

    find user on map

--- SoundOverlow/ViewController.swift
+++ SoundOverlow/ViewController.swift
@@ -7,13 +7,47 @@
 //
 
 import UIKit
+import MapKit
+import Foundation
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //MAP
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var map: MKMapView!
+    
+    let manager = CLLocationManager()  // setting up manager
+    
+    //this functions is called every time user moves
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.04, 0.04)
+        
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
+        
     }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
