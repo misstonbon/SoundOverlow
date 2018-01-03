a1bb70a3f84532fc8315d8c9f64b7783650abe93
Tanja Stroble <plusdeltah@gmail.com>
2018-01-02 16:52:07 -0800

    Initial Commit

---
+++ SoundOverlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundOverlow
+//
+//  Created by Tanja  Stroble on 1/2/18.
+//  Copyright © 2018 Tanja  Stroble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
